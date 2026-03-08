1c85fb0ba7d12c48e2c779dfa621f0ba034f4c91
Pete <138059854+psime@users.noreply.github.com>
2026-03-08 22:18:03 +0000

    added deploy config and other

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,60 @@ import "tabulator-tables/dist/css/tabulator.min.css";
 
 export default function App() {
   const [dbConnection, setDbConnection] = useState(null);
-  const [query, setQuery] = useState("SELECT * FROM events LIMIT 30;");
+  const [query, setQuery] = useState(
+    "SELECT * FROM events LIMIT 1000; -- this limit does not take into account Rows to return setting ",
+  );
+
   const [data, setData] = useState([]);
   const [columns, setColumns] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showEditor, setShowEditor] = useState(true);
+  const [eventCount, setEventCount] = useState(null);
+  const [rowLimit, setRowLimit] = useState(1000);
+  const [rowsPerPage, setRowsPerPage] = useState(20);
+
   const DEBUG_MODE = false;
+
+  const buttonStyle = {
+    margin: "6px",
+    padding: "8px 14px",
+    borderRadius: "6px",
+    border: "1px solid #ccc",
+    background: "#1f2937",
+    color: "#FFFFF0",
+    cursor: "pointer",
+  };
+
+  // updates query when rowLimit changes
+  useEffect(() => {
+    setQuery(`SELECT * FROM events LIMIT ${rowLimit};`);
+  }, [rowLimit]);
 
   useEffect(() => {
     async function initDuckDB() {
       try {
         // Create the DuckDB worker and WASM instance
         const logger = new duckdb.ConsoleLogger();
-        const worker = new Worker("/duckdb/duckdb-worker.js", { type: "module" });
+        const worker = new Worker(
+          `${import.meta.env.BASE_URL}duckdb/duckdb-worker.js`,
+          { type: "module" },
+        );
         const db = new duckdb.AsyncDuckDB(logger, worker);
-        await db.instantiate("/duckdb/duckdb.wasm");
-
+        // await db.instantiate("/duckdb/duckdb.wasm");
+        await db.instantiate(`${import.meta.env.BASE_URL}duckdb/duckdb.wasm`);
         // Connect to the database
         const connection = await db.connect();
 
         // Parquet files to load
-        const files = ['events', 'event_files', 'event_url_list'];
+        const files = ["events", "event_files", "event_url_list"];
 
         for (const name of files) {
-          const res = await fetch(`/data/${name}.parquet`);
+          // const res = await fetch(`/data/${name}.parquet`);
+          const res = await fetch(
+            `${import.meta.env.BASE_URL}data/${name}.parquet`,
+          );
           if (!res.ok) throw new Error(`Failed to fetch /data/${name}.parquet`);
-          
+
           const buffer = await res.arrayBuffer();
           const uint8Buffer = new Uint8Array(buffer); // <-- convert to Uint8Array
 
@@ -42,6 +71,10 @@ export default function App() {
         }
 
         setDbConnection(connection);
+
+        const res = await connection.query("SELECT COUNT(*) AS c FROM events");
+
+        setEventCount(res.toArray()[0].c);
         setLoading(false);
       } catch (err) {
         console.error("DuckDB init error:", err);
@@ -55,8 +88,7 @@ export default function App() {
   async function runQuery(sql) {
     if (!dbConnection) return;
 
-
-    const q = sql || query;   // use passed SQL or textarea
+    const q = sql || query; // use passed SQL or textarea
     try {
       const result = await dbConnection.query(q);
       const rows = result.toArray();
@@ -81,65 +113,87 @@ export default function App() {
 
   return (
     <div style={{ padding: 40, fontFamily: "sans-serif" }}>
-      <h2>DuckDB WASM + Parquet Browser Test</h2>
+      <h2>DuckDB WASM + Parquet Browser</h2>
+      <h4>Data from Statsbomb open data repo</h4>
+
+      <button onClick={() => setShowEditor(!showEditor)} style={buttonStyle}>
+        {showEditor ? "SQL Editor ▼" : "SQL Editor ▲"}
+      </button>
 
       {loading && <p>Loading DuckDB and Parquet files...</p>}
 
       {!loading && (
         <>
-          <textarea
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            style={{
-              width: "100%",
-              height: "150px",
-              fontFamily: "monospace",
-              fontSize: "14px",
-              marginBottom: "10px",
-            }}
-          />
+          {showEditor && (
+            <>
+              <textarea
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                style={{
+                  width: "100%",
+                  height: "150px",
+                  fontFamily: "monospace",
+                  fontSize: "14px",
+                  marginBottom: "10px",
+                }}
+              />
+
+              <br />
+              <button onClick={() => runQuery()} style={buttonStyle}>
+                Run Query Above
+              </button>
+
+              <hr />
+            </>
+          )}
           <br />
+          <label>Rows to return:</label>
 
-          <button onClick={() => runQuery()}
-                  style={{
-                  margin: "6px",
-                  padding: "8px 14px",
-                  borderRadius: "6px",
-                  border: "1px solid #ccc",
-                  background: "#1f2937",
-                  color: "white",
-                  cursor: "pointer",
-                  fontSize: "14px"
-                }}>Run Query
-          </button>
-          <hr />
-             <QueryButton
-              label="Events"
-              sql="SELECT * exclude(event_id) FROM events LIMIT 3000"
-              runQuery={runQuery}
-            />
+          <input
+            type="number"
+            value={rowLimit}
+            min="1000"
+            step="250"
+            onChange={(e) => setRowLimit(Number(e.target.value))}
+            style={{ marginLeft: "8px", width: "80px" }}
+          />
+
+          <label style={{ marginLeft: "20px" }}>Rows per page:</label>
+
+          <input
+            type="number"
+            value={rowsPerPage}
+            min="10"
+            step="5"
+            onChange={(e) => setRowsPerPage(Number(e.target.value))}
+            style={{ marginLeft: "8px", width: "80px" }}
+          />
+
+          <QueryButton
+            label={`Events (${eventCount ?? "..."})`}
+            sql={`SELECT * FROM events LIMIT ${rowLimit}`}
+            runQuery={runQuery}
+          />
+          <QueryButton
+            label="Events Summary"
+            sql="summarize events;"
+            runQuery={runQuery}
+          />
+          {DEBUG_MODE && (
+            <>
               <QueryButton
-              label="Events Summary"
-              sql="summarize events;"
-              runQuery={runQuery}
-            />
-            {DEBUG_MODE && (
+                label="Event Files"
+                sql="SELECT * FROM event_files LIMIT 100"
+                runQuery={runQuery}
+              />
               <QueryButton
-              label="Event Files"
-              sql="SELECT * FROM event_files LIMIT 100"
-              runQuery={runQuery}
-            />
-            )}
-           {DEBUG_MODE && (
-             <QueryButton
-              label="URL List"
-              sql="SELECT * FROM event_url_list LIMIT 100"
-              runQuery={runQuery}
-            />
-            )}             
-
-             <br />
-
+                label="URL List"
+                sql="SELECT * FROM event_url_list LIMIT 100"
+                runQuery={runQuery}
+              />
+            </>
+          )}
+          <br />
 
           <hr />
           {data.length > 0 && (
@@ -151,48 +205,40 @@ export default function App() {
                 movableColumns: true,
                 resizableRows: true,
                 pagination: "local",
-                paginationSize: 50,
+                paginationSize: rowsPerPage,
               }}
             />
           )}
-
         </>
       )}
     </div>
   );
 }
 
+// async function runQuery() {
+//   if (!dbConnection) return;
 
+//   try {
+//     const result = await dbConnection.query(query);
+//     const rows = result.toArray();
+//     setData(rows);
 
-  // async function runQuery() {
-  //   if (!dbConnection) return;
-
-  //   try {
-  //     const result = await dbConnection.query(query);
-  //     const rows = result.toArray();
-  //     setData(rows);
-
-  //     if (rows.length > 0) {
-  //       const cols = Object.keys(rows[0]).map((key) => ({
-  //         title: key,
-  //         field: key,
-  //         sorter: "string",
-  //       }));
-  //       setColumns(cols);
-  //     } else {
-  //       setColumns([]);
-  //     }
-  //   } catch (err) {
-  //     console.error("Query error:", err);
-  //     setData([]);
-  //     setColumns([]);
-  //   }
-  // }
-
-
-
-
-
+//     if (rows.length > 0) {
+//       const cols = Object.keys(rows[0]).map((key) => ({
+//         title: key,
+//         field: key,
+//         sorter: "string",
+//       }));
+//       setColumns(cols);
+//     } else {
+//       setColumns([]);
+//     }
+//   } catch (err) {
+//     console.error("Query error:", err);
+//     setData([]);
+//     setColumns([]);
+//   }
+// }
 
 // import { useEffect, useState } from "react";
 // import * as duckdb from "@duckdb/duckdb-wasm";
